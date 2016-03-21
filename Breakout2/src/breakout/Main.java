2fc81b92016818348e7d7e66b22186fe97cd5cee
ViviNygaard <140811@student.hin.no>
2016-03-21 01:20:08 +0100

    fix ball stuck + loss of life

---
+++ Breakout2/src/breakout/Main.java
@@ -0,0 +1,22 @@
+package breakout;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	
+private Game game;
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		game = new Game("Breakout", 800, 600, primaryStage);
+		game.init();
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
+
